438d0cd1156563381008c4c3b5c7fd76527c64b7
simsicon <simsicon@gmail.com>
2011-08-14 19:29:08 -0700

    no diff

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 dx = require('./index');
 util = require('util');
-dx.init('00001312', 'NzM2OTY3NzQwMDAzNzQ4OTMyOTY4MjE2');
-dx.request({number: '8615950519945', message: 'testtext'});
+dx.init('apid', 'key');
+dx.request({number: 'number', message: 'testtext'});
 
 
