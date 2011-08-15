f781c9ad494406a5055038cf2f0d2e0ff1965844
simsicon <simsicon@gmail.com>
2011-08-14 19:26:26 -0700

    diffs?

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 dx = require('./index');
 util = require('util');
-dx.init('you-apid', 'your-key');
-dx.request({number: 'your-number', message: 'testtext'});
+dx.init('00001312', 'NzM2OTY3NzQwMDAzNzQ4OTMyOTY4MjE2');
+dx.request({number: '8615950519945', message: 'testtext'});
 
 
