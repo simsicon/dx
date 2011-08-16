ed3acc4a2ff6ef0994cf704219831a810a7c7d68
simsicon <simsicon@gmail.com>
2011-08-15 23:09:53 -0700

    add a new base64 lib

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 dx = require('./index');
 util = require('util');
-dx.init('apid', 'key');
-dx.request({number: 'phonenumber', message: 'testtext'});
+dx.init('00001312', 'NzM2OTY3NzQwMDAzNzQ4OTMyOTY4MjE2');
+dx.request({number: '15950519945', message: 'testtext'});
 
 
